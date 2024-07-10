c4161a130fbd4e55d22ab82e0165316b6c7ff5bc
Zi0LEO <leonardo.npl.02@gmail.com>
2024-07-10 19:04:59 +0200

    added lsp plugins and automated treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,13 @@
--- treesitter
-
-return 
-    {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-    }
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+      auto_install = true,
+			ensure_installed = { "lua" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
